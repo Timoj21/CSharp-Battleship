6a6221ef2dcceed5e7c62b5dc61f26b1533cc969
Timo <timojenkins21@gmail.com>
2020-10-20 22:11:45 +0200

    [EDIT] kleine veranderingen om te proberen

--- CSharp-Battleship/GUI/ViewModels/GameViewModel.cs
+++ CSharp-Battleship/GUI/ViewModels/GameViewModel.cs
@@ -1,7 +1,11 @@
-﻿using GUI.Utils;
+﻿using GalaSoft.MvvmLight.Command;
+using GUI.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Input;
 
 namespace GUI.ViewModels
 {
@@ -10,10 +14,29 @@ namespace GUI.ViewModels
         private MainViewModel MainViewModel { get; set; }
         public string battlelogTextBlock { get; set; }
 
+        public ICommand gridButtonCommand { get; set; }
+
         public GameViewModel(MainViewModel mainViewModel)
         {
             this.MainViewModel = mainViewModel;
 
+            gridButtonCommand = new RelayCommand(() =>
+            {
+                GridButtonCommandHandler(gridButtonCommand);
+            });
+
+        }
+
+        private void GridButtonCommandHandler(object parameter)
+        {
+            switch(parameter.ToString())
+            {
+                case "A1":
+                    {
+                        battlelogTextBlock = "dit werkt";
+                        break;
+                    }
+            }
         }
     }
 }
